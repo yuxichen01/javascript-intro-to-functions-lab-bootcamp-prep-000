a3e4d10336e6d47487a956546a37c93affd7d9c7
yuxichen11@gmail.com <yuxichen11@gmail.com>
2018-04-15 03:21:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,14 @@ function sayHiToGrandma(string) {
     return "I love you, too."
   }
 }
+function sayHiToGrandma(string) {
+  var result = ""
+  if (string === "hello") {
+      result = "I can't hear you!"
+  } else if (string === "HELLO") {
+    result = "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+    result = "I love you, too." 
+  }
+  return result
+}
